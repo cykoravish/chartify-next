26fdb0fe6c3cb328900c2f337d9b83f16e40c60b
Ravish Bisht <ravishbisht85@gmail.com>
2024-12-06 19:26:39 +0530

    deployement debugging

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import NextAuth, { Session, User } from "next-auth";
+import NextAuth, { Session } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 import { compare } from "bcrypt";
 import { JWT } from "next-auth/jwt";
 
-// Define Custom types for JWT and Session
 interface CustomJWT extends JWT {
   id?: string;
 }
@@ -19,8 +18,7 @@ interface CustomSession extends Session {
   };
 }
 
-// Define authOptions with correct types
-const authOptions:any = {
+export const authOptions: any = {
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     CredentialsProvider({
@@ -74,9 +72,15 @@ const authOptions:any = {
     signIn: "/login",
   },
   callbacks: {
-    async jwt({ token, user }: { token: CustomJWT; user?: User }) {
+    // async jwt({ token, user }) {
+    //   if (user) {
+    //     token.id = user.id
+    //   }
+    //   return token
+    // },
+    async jwt({ token, user }: { token: CustomJWT; user?: any }) {
       if (user) {
-        token.id = user.id as string; // Add the user id to the token
+        token.id = user.id; // Add the user id to the token
       }
       return token;
     },
@@ -95,5 +99,6 @@ const authOptions:any = {
   },
 };
 
-// Export NextAuth handler as default
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
