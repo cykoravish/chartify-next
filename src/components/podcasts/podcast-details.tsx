b4ee87892c02c9549a319b9710fb3eaabcaef773
Ravish Bisht <ravishbisht85@gmail.com>
2024-12-17 18:08:11 +0530

    modal ui fix

--- src/components/podcasts/podcast-details.tsx
+++ src/components/podcasts/podcast-details.tsx
@@ -444,7 +444,7 @@ const socialPlatforms = [
                 whileHover={{ rotate: 90 }}
                 className="absolute top-0 right-0"
               >
-                <X className="h-6 w-6 text-gray-500 hover:text-gray-700" />
+                {/* <X className="h-6 w-6 text-gray-500 hover:text-gray-700" /> */}
               </motion.button>
             </DialogHeader>
 
